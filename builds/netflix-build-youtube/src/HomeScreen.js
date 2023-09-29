31e7b844576fe5af800a5d5ec111fb865e7161f0
Random123432 <catmanteleportex@gmail.com>
2023-09-29 15:54:26 +0100

    My netflix clone app built by me

---
+++ builds/netflix-build-youtube/src/HomeScreen.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './HomeScreen.css'
+import Nav from './Nav.js'
+import Banner from './Banner.js'
+import Row from './Row.js'
+
+function HomeScreen() {
+  return (
+    <div className='homescreen'>
+        <h1>jjfiyk</h1>
+        <Nav />
+        <Banner />
+        <Row />
+    </div>
+  )
+}
+
+export default HomeScreen
---
+++ builds/netflix-build-youtube/src/Nav.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Nav.css'
+
+function Nav() {
+  return (
+    <div className='nav'>
+        <img 
+          src='https://variety.com/wp-content/uploads/2020/05/netflix-logo.png?w=1024'
+          alt=""   
+        />
+
+        <img
+          src='' 
+          alt=''
+        />
+    </div>
+  )
+}
+
+export default Nav
